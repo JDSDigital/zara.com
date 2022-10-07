969b6237d21b7089b6b9de0a10fd9e023d8bb4fa
Jorge Daniel Sosa <JDSDigital@users.noreply.github.com>
2022-10-07 10:49:24 +0100

    Feat: Adds initial Layout component (#3)

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,22 @@
+import { FC, ReactNode } from 'react'
+
+import { Link } from 'react-router-dom'
+import styles from './Layout.module.css'
+
+export type LayoutProps = {
+  children: ReactNode
+}
+
+export const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <header className={styles.header}>
+        <Link className={styles.title} to={'/'}>
+          <span className={styles.title}>Podcaster</span>
+        </Link>
+      </header>
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  )
+}
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,1 +1,7 @@
-export const Home = () => <h1>Home</h1>
+import { Layout } from 'components/Layout'
+
+export const Home = () => (
+  <Layout>
+    <h1>Home</h1>
+  </Layout>
+)
