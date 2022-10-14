b08ad5a59b6666f5a65b47dd1311776accb75374
Jorge Daniel Sosa <JDSDigital@users.noreply.github.com>
2022-10-14 16:09:59 +0100

    Fix: Adds react query stale time (#13)

--- src/index.tsx
+++ src/index.tsx
@@ -12,10 +12,13 @@ import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
+const ONE_DAY = 1000 * 60 * 60 * 24
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      cacheTime: 1000 * 60 * 60 * 24 // 24 hours
+      cacheTime: ONE_DAY,
+      staleTime: ONE_DAY
     }
   }
 })
